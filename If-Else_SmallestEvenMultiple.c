171f812d97fc182adb228cae6149f3625270c394
shiva119127 <shiva.119127@gmail.com>
2026-03-15 23:43:08 +0530

    new commit

---
+++ If-Else_SmallestEvenMultiple.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int num1;
+    scanf("%d",&num1);
+    if(num1%2==0){
+        printf("%d",num1);
+    }
+    else{
+        printf("%d",2*num1);
+    }
+    
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
