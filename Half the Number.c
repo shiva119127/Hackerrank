fe8731d4959cca29d5a399f9fb00427ea91e8e16
shiva119127 <shiva.119127@gmail.com>
2026-03-22 22:59:30 +0530

    new commit

---
+++ Half the Number.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int num1;
+    scanf("%d",&num1);
+    float c=num1/2.0;
+    printf("Half of %d is %.2f",num1,c);
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
